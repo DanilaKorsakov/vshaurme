4a4d599c5899e323d515b11875ccc7317cddee07
Vitalya <devman-tasks@yandex.ru>
2018-10-15 17:21:04 +0300

    Создал систему ссылок для приложения

--- vshaurme/urls.py
+++ vshaurme/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('registration.urls')),
+    url(r'', include('messenger.urls')),
+    url(r'', include('wall.urls')),
 ]
