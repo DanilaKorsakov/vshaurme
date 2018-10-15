577e00868cd3d5d01056def891174625f90ab164
Vitalya <devman-tasks@yandex.ru>
2018-10-15 17:19:59 +0300

    Добавил модели постов и комментариев

--- wall/admin.py
+++ wall/admin.py
@@ -1,3 +1,6 @@
+from .models import Comment
+from .models import Post
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
--- wall/models.py
+++ wall/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.ForeignKey('auth.User')
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
